1eb1fd6f9bf7541d70cd3dda1baa2bc2c97bdbcb
Alanwang <44824234+alanwang97@users.noreply.github.com>
2018-12-05 10:23:50 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 library(stringr)
 library(dplyr)
 
+#this function can match the activities with their labels.
 match_feature <- function(x){
         if (str_trim(x)==1){
                 x <- "WALKING"
@@ -22,7 +23,8 @@ match_feature <- function(x){
         }
         return(x)
 }
-#this function can match the activities with their labels.
+
+#this function call the match_feature to replace the content in a file which only has the labels of the activities.
 y_match <- function(t){
         for (i in 1:nrow(t)){
                 
@@ -31,8 +33,8 @@ y_match <- function(t){
         }
         return(t)
 }
-#this function call the match_function to replace the content in a file which only has the labels of the activities.
 
+#this function can produce the mean of each col in a dataframe,and if the content of a column is all the same, it will not process the column.
 mean_col <- function(x){
         output <- vector()
         for (i in 1 : length(names(x)) ){
@@ -46,8 +48,8 @@ mean_col <- function(x){
         }
         return(as.vector(output))
 }
-#this function can produce the mean of each col in a dataframe,and if the content of a column is all the same, it will not process the column.
 
+#read the files
 train_x <- read.table("train/X_train.txt")
 train_y <- read.table("train/y_train.txt")
 train_subject <- read.table("train/subject_train.txt")
@@ -55,29 +57,29 @@ test_subject <- read.table("test/subject_test.txt")
 test_x <- read.table("test/X_test.txt")
 test_y <- read.table("test/y_test.txt")
 features <- read.table("features.txt")
-#read the fiels
 
+#replace labels with the real activities
+#4.Appropriately labels the data set with descriptive variable names.
 test_y_1 <- y_match(test_y)
 train_y_1 <- y_match(train_y)
-#replace labels with the real activities
 
-whole_test <- cbind(test_subject,test_y_1,test_x)
-names(whole_test) <- c("subject","active",as.character(features$V2))
-whole_train <- cbind(train_subject,train_y_1,train_x)
-names(whole_train) <- c("subject","active",as.character(features$V2))
 #use activities, subject, and features to generate dataframes which have all the data needed. 
+#3.Uses descriptive activity names to name the activities in the data set
+whole_test <- cbind(test_subject,test_y_1,test_x)
+whole_train <- cbind(train_subject,train_y_1,train_x)
+names(whole_test) <- c("subject","active",as.character(features$V2))
+names(whole_train) <- c("subject","active",as.character(features$V2))
 
-
+#2.Extracts only the measurements on the mean and standard deviation for each measurement.
 selected_train <- whole_train[,c(1,2,(grep("mean()|std()",as.character(features[,2])))+2)]
 selected_test <- whole_test[,c(1,2,(grep("mean()|std()",as.character(features[,2])))+2)]
-#Extracts only the measurements on the mean and standard deviation for each measurement.
 
+#1.merge the data of train and of the test
 selected_whole_data <- merge(selected_train,selected_test,all = TRUE)
-#merge the data of train and of the test
+
 
 # 5.From the data set in step 4, creates a second, independent tidy data set with the average 
 #   of each variable for each activity and each subject.
-
 answer<- data.frame()
 for (i in 1:length(unique(selected_whole_data$subject))){
         splited1_data <- split(selected_whole_data,selected_whole_data$subject)[[i]]
